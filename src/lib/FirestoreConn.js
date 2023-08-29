c50d6562b20fc349b0ea5b941a179bd0359c345f
PabloMur <polillomurillo@gmail.com>
2023-08-29 12:56:50 -0300

    s

--- src/lib/FirestoreConn.js
+++ src/lib/FirestoreConn.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const secretKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 const databaseURLSecretKey = process.env.DATABASEURL_SRECRETKEY;
 
-const firebaseApp = admin.apps.length
+admin.apps.length
   ? admin.app()
   : admin.initializeApp({
       credential: admin.credential.cert(secretKey),
