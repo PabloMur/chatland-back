153d14400d7f01e8ebfc1f6ca3399b9155e341ee
PabloMur <polillomurillo@gmail.com>
2023-08-29 13:09:39 -0300

    test

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -3,7 +3,8 @@ import { firestoreDB } from "@/lib/FirestoreConn";
 import bcrypt from "bcrypt";
 
 export async function POST(request) {
-  const { email, password } = await request.json();
+  const data = await request.json();
+  const { email, password } = data;
   const hashedPassword = await bcrypt.hash(password, 10);
   const docRef = firestoreDB.collection("auth").doc();
   await docRef.set({
