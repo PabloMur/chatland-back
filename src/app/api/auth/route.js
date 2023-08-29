07baa98683da8d9fef1320d64e9d350878359c1f
PabloMur <polillomurillo@gmail.com>
2023-08-29 12:21:34 -0300

    test de controller

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
-//import AuthController from "@/lib/controllers/authController";
+import AuthController from "@/lib/controllers/authController";
 
 export async function POST(request) {
   const body = await request.json();
+  const test = await AuthController.createAuth(request);
   return NextResponse.json(body);
 }
--- src/lib/controllers/authController.js
+++ src/lib/controllers/authController.js
@@ -1,47 +1,12 @@
-import AuthModel from "@/lib/models/AuthModel";
-import { headers } from "next/headers";
-import jwt from "jsonwebtoken";
+//import AuthModel from "@/lib/models/AuthModel";
+//import { headers } from "next/headers";
+//import jwt from "jsonwebtoken";
 
 class AuthController {
   static async createAuth(req) {
-    try {
-      const { email, password } = await req.json();
-      const created = await AuthModel.createAuth(email, password);
-      return created;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  static async deleteAuth(req) {
-    try {
-      const headersList = headers();
-      const authorizationRef = headersList.get("authorization");
-      if (!authorizationRef) {
-        return { error: "Missing authorization token" };
-      }
-
-      const token = authorizationRef.replace("Bearer ", "");
-      const secret = process.env.SECRET_KEY;
-      const decodedToken = jwt.verify(token, secret);
-
-      if (!decodedToken.email) {
-        return { error: "Invalid email in token" };
-      }
-
-      const authDeleted = await AuthModel.deleteAuth(decodedToken.email);
-      return authDeleted;
-    } catch (error) {
-      return { error: "An error occurred" };
-    }
-  }
-  static async retriveToken(req) {
-    try {
-      const { email, password } = await req.json();
-      const tokenCreated = await AuthModel.creatToken(email, password);
-      return tokenCreated ? tokenCreated : null;
-    } catch (error) {
-      console.error(error);
-    }
+    const { email, password } = await req.json();
+    const created = true; //await AuthModel.createAuth(email, password);
+    return created;
   }
 }
 
