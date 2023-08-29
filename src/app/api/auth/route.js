440a9739d633af7b3842b7eb3a009d64e823f7c3
PabloMur <polillomurillo@gmail.com>
2023-08-29 12:09:54 -0300

    saque controller

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import AuthController from "@/lib/controllers/authController";
+//import AuthController from "@/lib/controllers/authController";
 
 export async function POST(request) {
   const body = await request.json();
